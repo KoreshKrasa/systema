506bfabb3d4e08ae257bf4d774d375cc0e3c24f9
Arciom Krasilnikaŭ <koreshkrasa@gmail.com>
2025-07-02 19:49:33 +0300

    add niri image

--- build-niri.sh
+++ build-niri.sh
@@ -5,9 +5,7 @@ set -ouex pipefail
 RELEASE="$(rpm -E %fedora)"
 
 dnf5 -y copr enable atim/lazygit
-dnf5 -y copr enable ryanabx/cosmic-epoch
 dnf5 -y copr enable yalter/niri
-
 
 dnf5 -y remove firefox firefox-langpacks
 
@@ -15,12 +13,11 @@ dnf5 -y install fish helix steam-devices \
   libvirt jetbrains-mono-fonts nautilus gvfs-smb \
   lazygit ruff uv python3-lsp-server cargo rust-analyzer rustfmt \
   niri xwayland-satellite \
-  cosmic-desktop \
-  btop \
-  # gamescope
-
-dnf5 -y update
+  btop gdm \
 
 dnf5 -y install /tmp/gamescope-dbus.rpm /tmp/inputplumber.rpm /tmp/gamescope.rpm
+
+dnf5 -y copr disable atim/lazygit
+dnf5 -y copr disable yalter/niri
 
 systemctl enable podman.socket
