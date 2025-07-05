63dca726b642339b1469453817e0ac8dcd065f12
Arciom Krasilnikaŭ <koreshkrasa@gmail.com>
2025-07-05 12:23:54 +0300

    end test

--- build.sh
+++ build.sh
@@ -6,14 +6,12 @@ RELEASE="$(rpm -E %fedora)"
 
 dnf5 -y copr enable atim/lazygit
 dnf5 -y copr enable ryanabx/cosmic-epoch
-dnf5 -y copr enable yalter/niri
 
 dnf5 -y remove firefox firefox-langpacks
 
 dnf5 -y install fish helix steam-devices \
   libvirt jetbrains-mono-fonts nautilus gvfs-smb \
   lazygit ruff uv python3-lsp-server cargo rust-analyzer rustfmt \
-  niri xwayland-satellite \
 
 dnf5 -y update
 
@@ -22,6 +20,5 @@ dnf5 -y install /tmp/inputplumber.rpm /tmp/gamescope.rpm
 
 dnf5 -y copr disable atim/lazygit
 dnf5 -y copr disable ryanabx/cosmic-epoch
-dnf5 -y copr enable yalter/niri
 
 systemctl enable podman.socket
