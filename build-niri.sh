ab71443c84732a0d8a49a022f55677c5cae22296
Arciom Krasilnikaŭ <koreshkrasa@gmail.com>
2025-07-12 20:20:22 +0300

    niri - cosmic

--- build-niri.sh
+++ build-niri.sh
@@ -6,19 +6,21 @@ RELEASE="$(rpm -E %fedora)"
 
 dnf -y copr enable atim/lazygit
 dnf -y copr enable yalter/niri
+dnf -y copr enable solopasha/hyprland
+dnf -y copr enable lihaohong/yazi
 
 dnf -y remove firefox firefox-langpacks
 dnf -y install $(cat /tmp/pkgs | sed -e 's/\s*#.*$//' | sed -e '/^\s*$/d')
 
-dnf5 -y copr disable atim/lazygit
-dnf5 -y copr disable yalter/niri
+dnf -y copr disable atim/lazygit
+dnf -y copr disable yalter/niri
+dnf -y copr disable solopasha/hyprland
+dnf -y copr disable lihaohong/yazi
 
 systemctl enable podman.socket
 systemctl enable emptty.service
 
 mkdir /usr/lib/systemd/user/niri.service.wants
-ln -s /usr/lib/systemd/user/cosmic-bg.service /usr/lib/systemd/user/niri.service.wants
-ln -s /usr/lib/systemd/user/cosmic-panel.service /usr/lib/systemd/user/niri.service.wants
-ln -s /usr/lib/systemd/user/cosmic-app-library.service /usr/lib/systemd/user/niri.service.wants
-ln -s /usr/lib/systemd/user/cosmic-launcher.service /usr/lib/systemd/user/niri.service.wants
-ln -s /usr/lib/systemd/user/cosmic-settings-daemon.service /usr/lib/systemd/user/niri.service.wants
+ln -s /usr/lib/systemd/user/waybar.service /usr/lib/systemd/user/niri.service.wants
+ln -s /usr/lib/systemd/user/mako.service /usr/lib/systemd/user/niri.service.wants
+ln -s /usr/lib/systemd/user/swww.service /usr/lib/systemd/user/niri.service.wants
