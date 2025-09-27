5e93170b3929483f8bd4c8a6c4392c84f7a07e1c
Arciom Krasilnikaŭ <koreshkrasa@gmail.com>
2025-09-27 18:24:52 +0300

    use tuigreet

--- build/greeter.sh
+++ build/greeter.sh
@@ -1,2 +1,15 @@
-dnf --setopt=install_weak_deps=False install cosmic-greeter
-systemdctl enable cosmic-greeter.service
+# dnf -y --setopt=install_weak_deps=False install cosmic-greeter
+# systemdctl enable cosmic-greeter.service
+
+dnf -y install tuigreet
+
+greetd_config=$(cat <<EOF
+[terminal]
+vt = 1
+
+[default_session]
+command = "tuigreet --cmd niri-session"
+user = "greeter"
+EOF
+)
+echo "$greetd_config" > /etc/greetd/config.toml
