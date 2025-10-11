085148131ac383557f7649f767f77b9bc03b7f23
Arciom Krasilnikaŭ <koreshkrasa@gmail.com>
2025-10-11 09:39:17 +0300

    use niri-git

--- build/install-niri.sh
+++ build/install-niri.sh
@@ -1,4 +1,6 @@
 dnf -y copr enable yalter/niri-git
+echo "priority=1" | sudo tee -a /etc/yum.repos.d/_copr:copr.fedorainfracloud.org:yalter:niri-git.repo
+
 dnf -y install niri \
   xwayland-satellite \
   rofi-wayland \
