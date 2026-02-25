1f9f11b7beffd35f6c5e5ff6463a418362caebcf
Arciom Krasilnikaŭ <koreshkrasa@gmail.com>
2026-02-25 14:38:44 +0300

    switch to noctalia

--- build-niri/install-niri.sh
+++ build-niri/install-niri.sh
@@ -3,15 +3,15 @@ echo "priority=1" | sudo tee -a /etc/yum.repos.d/_copr:copr.fedorainfracloud.org
 
 dnf -y install niri \
   xwayland-satellite \
-  waybar \
-  rofi \
-  swww \
-  wlsunset
-dnf -y remove fuzzel
+  # waybar \
+  # rofi \
+  # swww \
+  # wlsunset
+dnf -y remove fuzzel mako
 dnf -y copr disable yalter/niri-git
 
 mkdir /usr/lib/systemd/user/niri.service.wants
 # ln -s /usr/lib/systemd/user/waybar.service /usr/lib/systemd/user/niri.service.wants
-ln -s /usr/lib/systemd/user/mako.service /usr/lib/systemd/user/niri.service.wants
-ln -s /usr/lib/systemd/user/swww.service /usr/lib/systemd/user/niri.service.wants
+# ln -s /usr/lib/systemd/user/mako.service /usr/lib/systemd/user/niri.service.wants
+# ln -s /usr/lib/systemd/user/swww.service /usr/lib/systemd/user/niri.service.wants
 ln -s /usr/lib/systemd/user/noctalia.service /usr/lib/systemd/user/niri.service.wants
