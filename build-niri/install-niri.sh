2354c5f524666b39d6ca150b83986b41e6e2e8e0
Arciom Krasilnikaŭ <koreshkrasa@gmail.com>
2026-02-28 09:21:43 +0300

    fix vicinae

--- build-niri/install-niri.sh
+++ build-niri/install-niri.sh
@@ -2,16 +2,10 @@ dnf -y copr enable yalter/niri-git
 echo "priority=1" | sudo tee -a /etc/yum.repos.d/_copr:copr.fedorainfracloud.org:yalter:niri-git.repo
 
 dnf -y install niri \
-  xwayland-satellite \
-  # waybar \
-  # rofi \
-  # swww \
-  # wlsunset
+  xwayland-satellite
 dnf -y remove fuzzel mako
 dnf -y copr disable yalter/niri-git
 
 mkdir /usr/lib/systemd/user/niri.service.wants
-# ln -s /usr/lib/systemd/user/waybar.service /usr/lib/systemd/user/niri.service.wants
-# ln -s /usr/lib/systemd/user/mako.service /usr/lib/systemd/user/niri.service.wants
-# ln -s /usr/lib/systemd/user/swww.service /usr/lib/systemd/user/niri.service.wants
+ln -s /usr/lib/systemd/user/vicinae.service /usr/lib/systemd/user/niri.service.wants
 ln -s /usr/lib/systemd/user/noctalia.service /usr/lib/systemd/user/niri.service.wants
