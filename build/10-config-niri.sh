0421cf129f0a8fd9011e13ef4d46b716d99a2524
Arciom Krasilnikaŭ <koreshkrasa@gmail.com>
2026-07-04 13:49:52 +0300

    disable vicinae by default

--- build/10-config-niri.sh
+++ build/10-config-niri.sh
@@ -1,3 +1,3 @@
 mkdir /usr/lib/systemd/user/niri.service.wants
-ln -s /usr/lib/systemd/user/vicinae.service /usr/lib/systemd/user/niri.service.wants
+# ln -s /usr/lib/systemd/user/vicinae.service /usr/lib/systemd/user/niri.service.wants
 ln -s /usr/lib/systemd/user/noctalia.service /usr/lib/systemd/user/niri.service.wants
