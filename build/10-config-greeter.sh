d68f0fb949df9643087e5cf20109a88832d0e222
Arciom Krasilnikaŭ <koreshkrasa@gmail.com>
2026-07-05 13:10:50 +0300

    use noctalia-greeter

--- build/10-config-greeter.sh
+++ build/10-config-greeter.sh
@@ -3,8 +3,10 @@ greetd_config=$(cat <<EOF
 vt = 1
 
 [default_session]
-command = "tuigreet --asterisks -r --cmd niri-session"
+command = "/usr/bin/noctalia-greeter-session -- --session niri-session"
 user = "greetd"
 EOF
 )
 echo "$greetd_config" > /etc/greetd/config.toml
+systemctl enable --force greetd
+systemctl set-default graphical.target
